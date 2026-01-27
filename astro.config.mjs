5a7ed938e6c326b28dc2d7259e5c1635c49fb6d0
zerok06 <jose.geeksjose@gmail.com>
2026-01-26 19:15:32 -0500

    feat: Implement Code of Conduct and Policies/Inclusion pages with new Navbar and Footer components, integrate Astro sitemap, and update `.gitignore`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://khipux.ilo.pe',
   vite: {
     plugins: [tailwindcss()],
   },
@@ -19,5 +20,5 @@ export default defineConfig({
     }
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
